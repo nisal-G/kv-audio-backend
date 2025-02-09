620caa2bd05bb83ddd1c3321792e2babcbb51ee4
nisal-G <nimgunathilaka021@gmail.com>
2025-02-09 12:34:43 +0530

    created review model, controller & router ✅

---
+++ controllers/reviewController.js
@@ -0,0 +1,6 @@
+import express from "express";
+import Review from "../models/review.js";
+
+export function addReview(req,res) {
+    
+}
---
+++ models/review.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema({
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    name : {
+        type : String,
+        required : true
+    },
+    rating : { 
+        type : Number,
+        required : true
+    },
+    comment : {
+        type : String,
+        required : true
+    },
+    date : {
+        type : Date,
+        required : true,
+        default : Date.now()
+    },
+    isApproved : {
+        type : Boolean,
+        required : true,
+        default : false
+    }
+})
+
+const Review = mongoose.model("Review", reviewSchema);
+
+export default Review;
