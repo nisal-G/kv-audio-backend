debaf7e83785ad7839284908d016383f98afc880
nisalG <nimgunathilaka021@gmail.com>
2025-12-08 18:49:06 +0530

    Remove debug log and enhance product fetching logic based on user role

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,8 +2,6 @@ import Product from "../models/product.js";
 
 export async function addProducts (req, res) {
 
-   console.log(req.user)
-
    if (req.user == null) {
     res.status(401).json({message : "Please Logging and try again"});
     return;
@@ -26,9 +24,20 @@ export async function addProducts (req, res) {
 }
 
 export async function getProducts (req, res) {
+
+    let isAdmin = false;
+    if(req.user != null && req.user.role === "Admin") {
+        isAdmin = true;
+    }
+
     try {
+        if(isAdmin) {
         const products = await Product.find();
         res.json(products);
+        } else {
+        const products = await Product.find({availability: true});
+        res.json(products);
+        }
 
     } catch (error) {
         res.status(500).json({message: "Failed to fetch products. Please try again."});
